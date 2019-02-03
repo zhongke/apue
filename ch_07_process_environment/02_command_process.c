802568b4f87186e59124ef3bd924dd9d1b87fe92
kekenow <zhongke@live.com>
2019-02-03 23:22:06 +0800

    Add exit handler

---
+++ ch_07_process_environment/02_command_process.c
@@ -0,0 +1,50 @@
+#include "../apue.h"
+
+#define TOK_ADD 5
+
+void do_line(char *);
+void cmd_add();
+int  get_token();
+
+int
+main()
+{
+        char line[MAXLINE];
+        while (fgets(line, MAXLINE, stdin) != NULL)
+                do_line(line);
+        exit(0);
+}
+
+char *tok_ptr;    /* global pointer for get_token() */
+
+void
+do_line(char *ptr)
+{
+        int cmd;
+        tok_ptr = ptr;
+
+        while ((cmd = get_token()) > 0) {
+                switch (cmd) {
+                case TOK_ADD: {
+                        cmd_add();
+                        break;
+                }
+                default:
+                        break;
+                }
+        }
+}
+
+void cmd_add()
+{
+        int token;
+        token = get_token();
+        /* rest of processing for this command */
+}
+
+int
+get_token()
+{
+        /* fetch next token from line pointed to by tok_ptr */
+        return 0;
+}
